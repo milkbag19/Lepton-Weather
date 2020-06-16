4ad232f6cdbc9a3fec06976905aeeefbe9429574
Josh Wells <40251012+milkbag19@users.noreply.github.com>
2020-06-16 15:56:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 
 router.get('/',(req, res) => {

e5a3d2178f791715803c07cab08c07136305c5c8
Josh Wells <40251012+milkbag19@users.noreply.github.com>
2020-06-16 00:57:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const url = require('url');
 const http = require('http');
 const express = require('express');
+var path = require('path');
 const app = new express();
 
 
 const server = app.listen(process.env.PORT || 8888, () => {
     app.get('/', function(request, response){
-        response.sendFile('frontend/index.html');
+        response.sendFile(path.dirname(require.main.filename)+'frontend/index.html');
     });
 });
 

521fb2da7d0cbcd185ed1cffb588a9839638658b
Josh Wells <40251012+milkbag19@users.noreply.github.com>
2020-06-16 02:01:24 -0400

    testing forms

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const express = require('express');
 var path = require('path');
 const app = new express();
 
+app.get('/', function(request, response){
+    console.log(__dirname);
+    response.sendFile('/app/frontend/index.html');
+});
 
 const server = app.listen(process.env.PORT || 8888, () => {
-    app.get('/', function(request, response){
-        console.log(__dirname);
-        response.sendFile('/app/frontend/index.html');
+    app.use(express.urlencoded());
+
+    app.post('/submit-form', (req, res) => {
+        const username = req.body.username
+
+        res.end()
     });
 });
 

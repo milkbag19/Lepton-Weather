999f3dcdc93d350eba224f1a657ed869c0b8d9a3
Josh Wells <40251012+milkbag19@users.noreply.github.com>
2020-06-16 15:19:36 -0400

    ejs

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ const url = require('url');
 const http = require('http');
 const express = require('express');
 var path = require('path');
+const ejs = require('ejs');
 const app = new express();
 const { body,validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
 
 app.get('/', function(request, response){
-    console.log(__dirname);
     response.sendFile('/app/frontend/index.html');
 });
+app.use(express.urlencoded({extended: false}));
 
+app.post('/', (req, res) => {
+    const username = req.body.username;
+    res.render('/app/frontend/index.html');
+    res.write(username);
+    res.end();
+});
+let people = ['geddy', 'neil', 'alex'],
+    html = ejs.render('<%= people.join(", "); %>', {people: people});
+// With jQuery:
+$('#output').html(html);
 const server = app.listen(process.env.PORT || 8888, () => {
-    app.use(express.urlencoded({extended: false}));
 
-    app.post('/', (req, res) => {
-        const username = req.body.username;
-        res.render('/app/frontend/index.html');
-        res.write(username);
-        res.end();
-    });
 });
 
 

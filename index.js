7317837c8d30a73e262e161cdb26dffe394b2b41
Josh Wells <40251012+milkbag19@users.noreply.github.com>
2020-06-16 00:45:17 -0400

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const url = require('url');
 const http = require('http');
+const express = require('express');
+const app = new express();
+
+
 const app = http.createServer( function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write('yeet');
     response.end();
 });
+app.get('/', function(request, response){
+    response.sendFile('./frontend/index.html');
+});
+
 
 app.listen(process.env.PORT || 8888);

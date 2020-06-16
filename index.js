c34c94719b52f6c640f2384106a24a3fa71565cb
Josh Wells <40251012+milkbag19@users.noreply.github.com>
2020-06-16 00:51:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ const express = require('express');
 const app = new express();
 
 
-const web = http.createServer( function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write('yeet');
-    response.end();
+const server = app.listen(7000, () => {
+    console.log(`Express running → PORT ${server.address().port}`);
 });
 app.get('/', function(request, response){
     response.sendFile('./frontend/index.html');
 });
 
-
-web.listen(process.env.PORT || 8888);

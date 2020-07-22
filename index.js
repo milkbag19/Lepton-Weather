f8e38cccd9efe073a59eb5e9a48f10f4cc7f2dc9
Josh Wells <40251012+milkbag19@users.noreply.github.com>
2020-07-21 21:20:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var url = require('url');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('hello world');
+    q = url.parse(req.url, true).query;
+
+    res.write(q.year + " " q.month);
     res.end();
 }).listen(process.env.PORT || 8080);
 

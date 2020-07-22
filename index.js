3f3b84fe6536190d948d51d4d976e8975d782834
Josh Wells <40251012+milkbag19@users.noreply.github.com>
2020-07-21 22:25:11 -0400

    :)

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-var http = require('http');
-var url = require('url');
+var mysql = require('mysql');
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    q = url.parse(req.url, true).query;
+var connection = mysql.createConnection
+({
+    host     : 'ftp.digiterahost.com',
+    user     : 'leadfeas_bruh',
+    password : 'JHTygzziLso3',
+    database : 'leadfeas_node'
+});
 
-    res.write(q.year + " " q.month);
-    res.end();
-}).listen(process.env.PORT || 8080);
+app.use(session({
+    secret: 'kj4h35b3487ytvedihb9832h',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+    var username = req.body.username;
+    var password = req.body.password;
+    if(username && password){
+        var query = mysql.query("SELECT * FROM `users` WHERE username = ? AND password = ?", [username, password], function(err, results, fields){
+            if(results.length > 0){
+                request.session.user_id = results.user_id;
+            } else {
+                response.send('Incorrect Username and/or Password!');
+            }
+            response.end();
+        });
+    } else {
+        response.send('Please fill in both fields');
+    }
+    response.end();
+});

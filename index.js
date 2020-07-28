faabaf6400464ef6815a68dc0716de680f43b211
Josh Wells <40251012+milkbag19@users.noreply.github.com>
2020-07-28 03:45:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.get('/', function(req, res) {
     if(req.session.id != null && req.session.id != ""){
+        console.log(req.session.id);
         res.render('pages/index', {});
     } else {
         res.render('pages/login', {});

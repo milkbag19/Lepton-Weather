7e84158641bc2c89e36a47f956609dbaf607a1b3
Josh Wells <40251012+milkbag19@users.noreply.github.com>
2020-07-28 03:36:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/auth', function(req, res) {
                 return console.error('error: ' + err.message);
             }
             if(results.length > 0){
-                req.session.user_id = results[0].user_id;
+                req.session.user_id = results[0].id;
                 console.log(results[0]);
                 res.writeHead(302, {
                     'Location': '/home'

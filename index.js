e08927ca878c3cbb054113c827c0d99157355440
Josh Wells <40251012+milkbag19@users.noreply.github.com>
2020-07-28 09:51:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.get('/home', function(request, response) {
 
 app.post('/results', function (req,res) {
     var local = req.body.location;
-    weather.find({search: local, degreeType: 'C'}, function (err, result) {
+    weather.find({search: 'ottawa', degreeType: 'C'}, function (err, result) {
         if (err) {
         } else if (result.length == 0) {
             return;
